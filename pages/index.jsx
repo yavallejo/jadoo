3ac2be772583e2490d9d47e0bd67d75153d60d65
Yan Vallejo <yavallejor@gmail.com>
2022-09-28 22:55:09 -0500

    feat: :construction: Install dependencies and initial structure

---
+++ pages/index.jsx
@@ -0,0 +1,9 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+
+export default function Home() {
+  return (
+    <h1 className="bg-slate-500 text-3xl hola--bien">Hello world!</h1>
+  )
+}
